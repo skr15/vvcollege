60713a558cb4cbe3856ce98bd8d0be97e05f3171
Sunny <sunnykr.1501@gmail.com>
2020-10-28 23:50:05 +0530

    changes

--- index.php
+++ index.php
@@ -657,7 +657,9 @@ while ($row = mysqli_fetch_array($res))
 </ul>
 </marquee>	
 </div>	
-<div style="margin-top:100px;margin-left:60px;">
+<div style="margin-top:-50px;margin-left:630px;width: 253px;
+	float: left;
+	margin-left: ;">	
 <h6 style="font-family: Arial, Helvetica, sans-serif;
     font-size: 18px;
     font-weight: normal;
@@ -666,7 +668,7 @@ while ($row = mysqli_fetch_array($res))
     text-align: center;
     width: 274px;
     height: 40px;
-    padding-top: 14px;margin-left:-37px;">Contact Information</h6>
+    padding-top: 14px;margin-left:-65px;">Contact Information</h6>
 <div style="margin-top:-5px;margin-left:-61px;height:18em;width:265px;-moz-border-radius: 1em 4em 1em 4em;
 border-radius: 0em 0em 2em 2em;
 border-bottom: 1px  black solid;border-left: 1px  black solid;border-right: 1px  black solid;color:#454545;text-align:left">
